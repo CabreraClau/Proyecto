bac3ff2b6dfec6fc2a81bf42941123df4529bfb4
CabreraClau <claudio.cabrera@correo.ucu.edu.uy>
2023-05-16 18:48:52 -0300

    claudio

---
+++ src/Library/NormalUser.cs
@@ -0,0 +1,28 @@
+﻿namespace Library;
+public class NormalUser : Person
+{
+    public NormalUser(string nombre, string apellido, int ci, int telefono, string correo, string direccion): base(nombre, apellido, ci, telefono, correo, direccion)
+    {
+
+    }
+    public void CalificarRefugio(Refugio refugio, int calificacion)
+    {
+
+    }
+    public void AdoptarAnimal(Refugio refugio, Animal animal)
+    {
+
+    }
+    public void ColaborarRefugio(Refugio refugio, float monto)
+    {
+
+    }
+    public List<Refugio> BuscarRefugio (string criterioBusqueda)
+    {
+
+
+        return new list<Refugio>();
+    }
+}
+
+
---
+++ src/Library/Person.cs
@@ -0,0 +1,30 @@
+using System;
+namespace Library;
+
+
+public class Person
+{
+    public string Nombre {get; }
+    public string Apellido {get ;}
+    public int Identidad {get;}
+    public int Telefono {get ; set;}
+    public string Correo {get; set;}
+    public string Direccion {get ; set;}
+    
+    public Person(string nombre, string apellido, int identidad, int telefono, string correo, string direccion)
+    {
+        this.Nombre = nombre;
+        this.Apellido = apellido;
+        this.Identidad = identidad;
+        this.Telefono = telefono;
+        this.Correo = correo;
+        this.Direccion = direccion;
+
+    }
+    
+
+    public void ContactarRefugios(Refugio refugio)
+    {
+
+    }
+}
