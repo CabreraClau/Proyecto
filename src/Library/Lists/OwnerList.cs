fef59c2f97c172b1208d911200db3607d7799bc3
belenferreir <beluferrerey@gmail.com>
2023-05-17 15:45:08 -0300

    B

--- src/Library/Lists/OwnerList.cs
+++ src/Library/Lists/OwnerList.cs
@@ -4,8 +4,47 @@ namespace Library;
 public class OwnerList
 {
     public IList<Owner> owners = new List<Owner>();
-    public IList<Owner> Owner;
-    
-    
+    public IList<Owner> Owner
+    {
+        get
+        {
+            return new ReadOnlyCollection<Owner>(this.owners);
+        }
+    }
+    public void AddOwner(Owner owner)
+    {
+      this.owners.Add(owner);
+    }
+    public bool IdExistence(long Id)
+    {
+      for (int i= 0; i < this.owners.Count; i++)
+        {
+            if (owners[i].Id == Id)
+            {
+                return true;
+            }
+        }
+      return false;
+    }
+    public int GetIndexById(long Id)
+    {
+        for (int Owner = 0;  Owner < this.owners.Count; Owner ++)
+        {
+            if (owners[Owner].Id == Id)
+            {
+                return Owner;
+            }
+        }
+        return -1;
+    }
+    public Owner GetOwnerById(long Id)
+    {
+        int index = GetIndexById(Id);
+        return this.owners[index];
+    }
 
+    public void Clear()
+    {
+        this.owners.Clear();
+    }
 }
